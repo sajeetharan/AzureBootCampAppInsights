cdd77e1a2ae711f45467046f372fc0a00f33714d
sajeetharan <sajeetharan@outlook.com>
2018-04-22 06:57:00 +0530

    Final Commit

---
+++ AppInsightsDemo/AzureBootCampAppInsights/AzureBootCampAppInsights/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AzureBootCampAppInsights.Models;
+using Microsoft.Extensions.Logging;
+
+namespace AzureBootCampAppInsights.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            _logger.LogWarning("Someone from azure bootcamp opened about page!");
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
